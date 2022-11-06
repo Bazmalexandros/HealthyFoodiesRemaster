b4e92f796cc737471227de4ca4b7bfe64d317c5f
MiguelDIslas <miguelangelde15@gmail.com>
2022-11-06 01:32:33 -0700

    change

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { API_KEY, API_ID} from "@env";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: "healthyfoodiestec.firebaseapp.com",
-  projectId: "healthyfoodiestec",
-  storageBucket: "healthyfoodiestec.appspot.com",
-  messagingSenderId: "367804159258",
-  appId: API_ID
-};
+    apiKey: API_KEY,
+    authDomain: "healthyfoodies-1d8e8.firebaseapp.com",
+    projectId: "healthyfoodies-1d8e8",
+    storageBucket: "healthyfoodies-1d8e8.appspot.com",
+    messagingSenderId: "335664167814",
+    appId: API_ID
+  };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
